7b02abfeed4badb34b33320cf143e80960cca544
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2022-10-16 13:13:06 +0700

    Giai đoạn từ máy tính cá nhân lên github

--- index.php
+++ index.php
@@ -18,3 +18,12 @@ Các công đoạn làm việc với git
 2. Kiểm tra sự thay đổi ở trên phần mềm github desktop
 3. Đóng gói code lại và dán nhãn (commit)
 4. Public repository (Tạo thư mục trên github bằng github desktop) và đẩy phần code vừa rồi lên
+
+
+Nếu tạo thư mục code mới và muốn đẩy lên git
+1. Thực hiện code bình thường
+2. Vào github desktop chọn File -> Add local repository -> Chọn thư mục đang code
+3. Bấm vào chữ nhỏ "create a repository" để git biết là mình muốn quản lý thư mục này
+4. Github desktop sẽ tự đóng gói code đang có vào 1 commit "Initial commit"
+5. Public repository: tạo tên thư mục muốn lưu ở github sau đó bấm -> có code trên github
+
