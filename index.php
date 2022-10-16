0ebce8ff5ad1530acae517103961986221ef1158
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2022-10-16 12:57:27 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!-- Code chức năng cho trang web -->
+
+Git sẽ chia công việc ra thành các nhánh
+Khi đưa git vào 1 thư mục thì mặc định 1 nhánh gốc sẽ được tạo ra
+
+        master
+          |
+         /|
+        / |\
+Tạo mới   | \ Danh sách user
+
+
+
+Có 2 chiều làm việc: Github <====> Thư mục ở local (git local)
