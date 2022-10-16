136861a0599409adb99609e7cf2c53a13815e7b3
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2022-10-16 13:03:21 +0700

    Tên chức năng được dán nhãn

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Git sẽ chia công việc ra thành các nhánh
 Khi đưa git vào 1 thư mục thì mặc định 1 nhánh gốc sẽ được tạo ra
 
-        master
+        master / main
           |
          /|
         / |\
@@ -12,3 +12,7 @@ Tạo mới   | \ Danh sách user
 
 
 Có 2 chiều làm việc: Github <====> Thư mục ở local (git local)
+
+Các công đoạn làm việc với git
+1. Code ở trên 1 nhánh nào đó
+2. Kiểm tra sự thay đổi ở trên phần mềm github desktop
